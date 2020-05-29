3274600ea371e49dd9949865be0071dd5490e985
ffio1 <ffioathome@gmail.com>
2020-05-29 10:28:05 -0700

    Added Direct X 9 Headers
    Added headers to prevent the compiler from needing to install the 1.2 GB Direct X SDK (as they only need less than 1 MB)

--- RSCDLCEnabler/Functions.h
+++ RSCDLCEnabler/Functions.h
@@ -1,6 +1,6 @@
 #include "windows.h"
-#include <d3d9.h>
-#include <d3dx9.h>
+#include "DirectX/d3d9.h"
+#include "DirectX/d3dx9.h"
 
 #pragma comment(lib, "d3d9.lib")
 #pragma comment(lib, "d3dx9.lib")
