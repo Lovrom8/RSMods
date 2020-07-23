bd488aa83ad50ea852cfd986315740882266de99
ffio1 <ffioathome@gmail.com>
2020-07-22 17:13:18 -0700

    Main mod conversion (!UNTESTED!)
    Still needs ImGUI and settings -> function.

--- RSCDLCEnabler/GuitarSpeak.cpp
+++ RSCDLCEnabler/GuitarSpeak.cpp
@@ -2,6 +2,11 @@
 
 cGuitarSpeak GuitarSpeak;
 
+/*
+Modified / Translated from the Visual Basic code provided by UKLooney
+Open source here: https://cdn.discordapp.com/attachments/711634414240530462/735574443035721798/G2RS_v0.3_source.zip
+*/
+
 int cGuitarSpeak::NoteToHex(char* note, int octave){
 	int notePlayed = 0x0;
 	octave = octave - 2; // The first full set of notes is at the 2nd octave so we do the math as if they were our zero-point.
@@ -37,3 +42,204 @@ int cGuitarSpeak::NoteToHex(char* note, int octave){
 	return notePlayed;
 }
 
+byte cGuitarSpeak::GetCurrentNote() {
+	return *(byte*)MemUtil.FindDMAAddy(Offsets.ptr_guitarSpeak, Offsets.ptr_guitarSpeakOffets);
+}
+
+
+
+void cGuitarSpeak::TimerTick() {
+	while (true) {
+		Sleep(timer);
+		currentForegroundWindow = GetForegroundWindow();
+
+		if (lastForegroundWindow != currentForegroundWindow) { // Check if the foreground window has changed since the last timer.
+			lastForegroundWindow = currentForegroundWindow;
+
+			GetWindowTextA(currentForegroundWindow, windowTitle, 16);
+
+			if (!isRocksmithFocused) { // Is Rocksmith in the background?
+				if (windowTitle == (LPSTR)"Rocksmith 2014") {
+					isRocksmithFocused = true;
+					sendKeystrokesToRS2014 = false;
+					if (!isRocksmithOpen) { // Is Rocksmith closed?
+						isRocksmithOpen = true; // Well obviously if we see the window is called "Rocksmith 2014" and this code is running... Rocksmith is open :P
+						RocksmithProcessHandle = OpenProcess(0x10, false, GetProcessId(FindWindowA(NULL, "Rocksmith 2014")));
+					}
+				}
+			}
+			else { // If Rocksmith still focused?
+				isRocksmithFocused = false;
+				sendKeystrokesToRS2014 = false;
+				seqListenCount = 0;
+			}
+		}
+
+		if (!isRocksmithFocused) // If rocksmith isn't focused, don't try to read the notes.
+			return;
+
+		// Pointer 1
+		ReadProcessMemory(RocksmithProcessHandle, (LPCVOID)Offsets.ptr_guitarSpeak, (LPVOID)memBuffer, (SIZE_T)memBufferLength, (SIZE_T*)bytesRead);
+		pointer1 = memBuffer[0] || (memBuffer[1] << 0x8) || (memBuffer[2] << 0x10) || (memBuffer[3] << 0x18) + Offsets.ptr_guitarSpeakOffets[0];
+
+		// Pointer 2
+		ReadProcessMemory(RocksmithProcessHandle, (LPCVOID)pointer1, (LPVOID)memBuffer, (SIZE_T)memBufferLength, (SIZE_T*)bytesRead);
+		pointer2 = memBuffer[0] || (memBuffer[1] << 0x8) || (memBuffer[2] << 0x10) || (memBuffer[3] << 0x18) + Offsets.ptr_guitarSpeakOffets[1];
+
+		// Pointer 3
+		ReadProcessMemory(RocksmithProcessHandle, (LPCVOID)pointer2, (LPVOID)memBuffer, (SIZE_T)memBufferLength, (SIZE_T*)bytesRead);
+		pointer3 = memBuffer[0] || (memBuffer[1] << 0x8) || (memBuffer[2] << 0x10) || (memBuffer[3] << 0x18) + Offsets.ptr_guitarSpeakOffets[2];
+
+		// MemoryLocNote
+		ReadProcessMemory(RocksmithProcessHandle, (LPCVOID)pointer3, (LPVOID)memBuffer, (SIZE_T)memBufferLength, (SIZE_T*)bytesRead);
+		memoryLocNote = memBuffer[0] || (memBuffer[1] << 0x8) || (memBuffer[2] << 0x10) || (memBuffer[3] << 0x18);
+
+		int mem = memBuffer[0];
+
+		if (mem != 0xFF && mem >= midiNotesArray->size()) { // Is the note too high pitched?
+			return;
+		}
+
+		if (mem == lastNoteBuffer && mem != currentNoteBuffer)
+			currentNoteBuffer = mem;
+		if (mem != lastNoteBuffer)
+			lastNoteBuffer = mem;
+
+		if (currentNoteBuffer != 0xFF && currentNoteBuffer != lastNote) { // Check if there is a new note
+			newNote = true;
+			note = currentNoteBuffer;
+			lastNote = note;
+		}
+		if (currentNoteBuffer == 0xFF && lastNote != 0x0) { // If the note ends
+			currentNote = "-";
+			currentNoteOctave = -1;
+			newNote = false;
+			note = 0;
+			lastNote = 0;
+		}
+
+		if (newNote) {
+			std::string strSendTemp = "";
+			currentNote = midiNotesArray[note];
+			currentNoteOctave = midiNotesOctaveArray[note];
+
+			if (note != 0) { // A switch / case setup would be 100x better, but it kept telling me I couldn't call a "this." in this function... I'm not calling that...
+				if (note == NoteToHex("A", 2))
+					strSendTemp = strKey_A;
+				else if (note == NoteToHex("Bb", 2))
+					strSendTemp = strKey_Bb;
+				else if (note == NoteToHex("B", 2))
+					strSendTemp = strKey_B;
+				else if (note == NoteToHex("C", 3))
+					strSendTemp = strKey_C;
+				else if (note == NoteToHex("C#", 3))
+					strSendTemp = strKey_Cs;
+				else if (note == NoteToHex("D", 3))
+					strSendTemp = strKey_D;
+				else if (note == NoteToHex("Eb", 3))
+					strSendTemp = strKey_Eb;
+				else if (note == NoteToHex("E", 3))
+					strSendTemp = strKey_E;
+				else if (note == NoteToHex("F", 3))
+					strSendTemp = strKey_F;
+				else if (note == NoteToHex("F#", 3))
+					strSendTemp = strKey_Fs;
+				else if (note == NoteToHex("G", 3))
+					strSendTemp = strKey_G;
+				else if (note == NoteToHex("G#", 3))
+					strSendTemp = strKey_Gs;
+				else
+					strSendTemp = "";
+			}
+
+			if (!notesUsedArray->find(currentNote)) {
+				return;
+			}
+
+			if (sendKeystrokesToRS2014)
+				strSend = strSendTemp;
+			else {
+				if (newNote) {
+					if (note == seqArrayNotes[seqListenCount]) {
+						seqListenCount++;
+						if (seqListenCount > seqArrayNotesLength - 1) {
+							seqListenCount = 0;
+							sendKeystrokesToRS2014 = true;
+						}
+					}
+					else if (note == seqArrayNotes[0])
+						seqListenCount = 1;
+					else
+						seqListenCount = 0;
+				}
+			}
+		}
+		newNote = false;
+
+		if (sendKeystrokesToRS2014) { // Are we sending a keystroke RIGHT now
+			if (strSend.find("{", 0)) {
+
+				// White Space Characters
+				if (strSend == "{SPACE}") {
+					PostMessage(FindWindow(NULL, L"Rocksmith 2014"), WM_KEYDOWN, VK_SPACE, 0);
+					Sleep(30);
+					PostMessage(FindWindow(NULL, L"Rocksmith 2014"), WM_KEYUP, VK_SPACE, 0);
+				}
+				
+				else if (strSend == "{ENTER}") {
+					PostMessage(FindWindow(NULL, L"Rocksmith 2014"), WM_KEYDOWN, VK_RETURN, 0);
+					Sleep(30);
+					PostMessage(FindWindow(NULL, L"Rocksmith 2014"), WM_KEYUP, VK_RETURN, 0);
+				}
+				else if (strSend == "{TAB}") {
+					PostMessage(FindWindow(NULL, L"Rocksmith 2014"), WM_KEYDOWN, VK_TAB, 0);
+					Sleep(30);
+					PostMessage(FindWindow(NULL, L"Rocksmith 2014"), WM_KEYUP, VK_TAB, 0);
+				}
+
+				// Navigation Keys 
+
+				else if (strSend == "{PGUP}") {
+					PostMessage(FindWindow(NULL, L"Rocksmith 2014"), WM_KEYDOWN, VK_PRIOR, 0);
+					Sleep(30);
+					PostMessage(FindWindow(NULL, L"Rocksmith 2014"), WM_KEYUP, VK_PRIOR, 0);
+				}
+				else if (strSend == "{PGDN}") {
+					PostMessage(FindWindow(NULL, L"Rocksmith 2014"), WM_KEYDOWN, VK_NEXT, 0);
+					Sleep(30);
+					PostMessage(FindWindow(NULL, L"Rocksmith 2014"), WM_KEYUP, VK_NEXT, 0);
+				}
+				
+				
+				// Arrow Keys
+				else if (strSend == "{UP}") {
+					PostMessage(FindWindow(NULL, L"Rocksmith 2014"), WM_KEYDOWN, VK_UP, 0);
+					Sleep(30);
+					PostMessage(FindWindow(NULL, L"Rocksmith 2014"), WM_KEYUP, VK_UP, 0);
+				}
+				else if (strSend == "{DOWN}") {
+					PostMessage(FindWindow(NULL, L"Rocksmith 2014"), WM_KEYDOWN, VK_DOWN, 0);
+					Sleep(30);
+					PostMessage(FindWindow(NULL, L"Rocksmith 2014"), WM_KEYUP, VK_DOWN, 0);
+				}
+
+				// Misc
+				else if (strSend == "{ESC}") {
+					PostMessage(FindWindow(NULL, L"Rocksmith 2014"), WM_KEYDOWN, VK_ESCAPE, 0);
+					Sleep(30);
+					PostMessage(FindWindow(NULL, L"Rocksmith 2014"), WM_KEYUP, VK_ESCAPE, 0);
+				}
+
+				else { // Non-predetermined key
+					strSend = ""; // Reset mapping
+					return;
+				}
+
+			}
+			else if (strSend == "<CANCEL>") // User wants to stop using GuitarSpeak mod for the current time.
+				sendKeystrokesToRS2014 = false;
+		}
+	}
+}
+
+// ToDo: Convert Line 405-543 (checkbox-> strKey) to enable the mod. Needs some work done with ImGUI.
--- RSCDLCEnabler/GuitarSpeak.h
+++ RSCDLCEnabler/GuitarSpeak.h
@@ -1,16 +1,56 @@
 #pragma once
 
 #include "windows.h"
+#include "MemUtil.h"
 #include "Offsets.h"
 #include <vector>
 #include <string>
+#include <sstream>
 #include <iostream>
 
 class cGuitarSpeak {
 public:
 	int NoteToHex(char* note, int octave);
+	byte GetCurrentNote();
+	void TimerTick();
 private:
-	int pointer1, pointer2, pointer3, memoryLocNote, bytesread;
+	int pointer1, pointer2, pointer3, memoryLocNote, bytesRead;
 	int lastNoteBuffer = 0xFF, currentNoteBuffer = 0xFF;
-	byte memBuffer;
+	int timer = 50; // Milliseconds | Original value of 50ms
+	int* memBuffer = new int[memBufferLength]{ 0, 0, 0, 0 };
+	int memBufferLength = 4;
+	bool isRocksmithFocused = false;
+	bool isRocksmithOpen = false;
+	bool sendKeystrokesToRS2014 = false;
+	bool isThisNewNote = false;
+	bool newNote = false;
+	HANDLE RocksmithProcessHandle;
+	HWND lastForegroundWindow, currentForegroundWindow;
+	byte note, lastNote;
+	LPSTR windowTitle;
+
+
+	// Potentially garbage values we can break down to save space / memory
+
+	int timerCount, memberCount;
+	int seqListenCount = 0;
+	std::string* keyComboArray = new std::string[12]{ "DELETE", "DOWN", "ENTER", "ESCAPE", "PAGE DN", "PAGE UP", "SPACE", "TAB", "UP", "CANCEL", "none", "TBD" };
+	std::string* keyPressArray = new std::string[12]{ "{DELETE}", "{DOWN}", "{ENTER}", "{ESC}", "{PGDN}", "{PGUP}", "{SPACE}", "{TAB}", "{UP}", "<CANCEL>", "<none>", "<TBD>" };
+	std::string* notesUsedArray = new std::string[6]{ "A", "B", "C#", "Eb", "F", "G" };
+	std::string* notesSeqTextArray = new std::string[12]{ "A 2", "B 2", "C# 3", "Eb 3", "F 3", "G 3", "A 3", "B 3", "C# 4", "Eb 4", "F 4", "G 4" };
+	int* notesSeqMidiArray = new int[12]{0x2D, 0x2F, 0x31, 0x33, 0x35, 0x37, 0x39, 0x3B, 0x3D, 0x3F, 0x41, 0x43};
+	int* seqArrayNotes = new int[seqArrayNotesLength]{ 0, 0, 0, 0, 0 };
+	int seqArrayNotesLength = 5;
+	std::string strSend = "";
+	int* midiArray = new int[96]{ 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x90, 0x91, 0x92, 0x93, 0x94, 0x95 }; // C-1 <-> C6
+	std::string* midiNotesArray = new std::string[96]{ "C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "Bb", "B", "C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "Bb", "B", "C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "Bb", "B", "C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "Bb", "B", "C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "Bb", "B", "C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "Bb", "B", "C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "Bb", "B", "C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "Bb", "B" }; // Note Names
+	std::string* midiNotesFullArray = new std::string[96]{ "C-1", "C#-1", "D-1", "Eb-1", "E-1", "F-1", "F#-1", "G-1", "G#-1", "A-1", "Bb-1", "B-1", "C0", "C#0", "D0", "Eb0", "E0", "F0", "F#0", "G0", "G#0", "A0", "Bb0", "B0", "C1", "C#1", "D1", "Eb1", "E1", "F1", "F#1", "G1", "G#1", "A1", "Bb1", "B1", "C2", "C#2", "D2", "Eb2", "E2", "F2", "F#2", "G2", "G#2", "A2", "Bb2", "B2", "C3", "C#3", "D3", "Eb3", "E3", "F3", "F#3", "G3", "G#3", "A3", "Bb3", "B3", "C4", "C#4", "D4", "Eb4", "E4", "F4", "F#4", "G4", "G#4", "A4", "Bb4", "B4", "C5", "C#5", "D5", "Eb5", "E5", "F5", "F#5", "G5", "G#5", "A5", "Bb5", "B5", "C6", "C#6", "D6", "Eb6", "E6", "F6", "F#6", "G6", "G#6", "A6", "Bb6", "B6" };
+	int* midiNotesOctaveArray = new int[96]{ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6 };
+	int indexKey_A, indexKey_Bb, indexKey_B, indexKey_C, indexKey_Cs, indexKey_D, indexKey_Eb, indexKey_E, indexKey_F, indexKey_Fs, indexKey_G, indexKey_Gs;
+	std::string strKey_A, strKey_Bb, strKey_B, strKey_C, strKey_Cs, strKey_D, strKey_Eb, strKey_E, strKey_F, strKey_Fs, strKey_G, strKey_Gs;
+	std::string reqSeq1, reqSeq2, reqSeq3, reqSeq4, reqSeq5;
+	std::string currentNote = "-";
+	int currentNoteOctave = -1;
+	
+
 };
