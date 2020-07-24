d24c0110b2337d3590b07610a660940a2bafc2ca
Rory McLean <zagatozee@hotmail.com>
2020-07-24 13:38:50 -0500

    Notes for replacement files added

---
+++ RSCDLCEnabler/Files.cpp
@@ -0,0 +1,8 @@
+/*
+cache_psarc\cache7.7z\manifests \ tuningdatabase.json - full replacement with our file - "ideally" ability to add to it one entry at a time from the gui.... but low priority.
+
+cache_psarc\cache4.7z\gfxassets\views \ introsequence.gfx - full replacement
+
+cache_psarc\cache4.7z\localization \ maingame.csv - lines to be appended to existing file (base file is 8 mb), pipe dream is gui add entries to match tuningdatabase.json
+
+*/
