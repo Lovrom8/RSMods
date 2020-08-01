c21a60ab8ff9311f970e1503801ac2b9cdc945cf
ffio1 <ffioathome@gmail.com>
2020-07-31 21:55:35 -0700

    Guitar Speak stops in Tuning / Song / & Lesson menus

--- RSCDLCEnabler/GuitarSpeak.h
+++ RSCDLCEnabler/GuitarSpeak.h
@@ -12,9 +12,6 @@
 #include "MemUtil.h"
 #include "Settings.h"
 
-#include "DirectX/d3dx9.h"
-#include "D3D.h"
-
 class cGuitarSpeak {
 public:
 	byte GetCurrentNote();
@@ -31,6 +28,41 @@ private:
 	bool newNote = false;
 	std::string* strKeyList = new std::string[96]; // 12 notes in an octave, 8 octaves spanned (-1 <-> 6)
 	std::string strSend;
+
+
+	std::string cGuitarSpeak::tuningMenus[17] = { // These are all the menus where you need to tune
+	(std::string)"SelectionListDialog",
+	(std::string)"LearnASong_PreSongTuner",
+	(std::string)"LearnASong_PreSongTunerMP",
+	(std::string)"LearnASong_SongOptions",
+	(std::string)"NonStopPlay_PreSongTuner",
+	(std::string)"NonStopPlay_PreSongTunerMP",
+	(std::string)"ScoreAttack_PreSongTuner",
+	(std::string)"SessionMode_PreSMTunerMP",
+	(std::string)"SessionMode_PreSMTuner",
+	(std::string)"UIMenu_Tuner",
+	(std::string)"UIMenu_TunerMP",
+	(std::string)"Guitarcade_Tuner",
+	(std::string)"Tuner",
+	(std::string)"Duet_PreSongTuner",
+	(std::string)"H2H_PreSongTuner",
+	(std::string)"GETuner",
+	(std::string)"PreGame_GETuner"
+	};
+
+	std::string  cGuitarSpeak::songModes[8] = { // These are all the menus where you would play guitar games.
+		(std::string)"LearnASong_Game",
+		(std::string)"NonStopPlay_Game",
+		(std::string)"ScoreAttack_Game",
+		(std::string)"LearnASong_Pause",
+		(std::string)"NonStopPlay_Pause",
+		(std::string)"ScoreAttack_Pause",
+	};
+
+	std::string  cGuitarSpeak::lessonModes[2] = { // These are the Guided Experience / Lessons modes.
+		(std::string)"GuidedExperience_Game",
+		(std::string)"GuidedExperience_Pause"
+	};
 };
 
 extern cGuitarSpeak GuitarSpeak;
