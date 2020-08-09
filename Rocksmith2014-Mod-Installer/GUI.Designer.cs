f2f2b515baedb0469533d8791afcf5e0494bbe21
ffio1 <ffioathome@gmail.com>
2020-08-09 00:30:35 -0700

    No more *DLC references
    Sorry Lovro :(

---
+++ Rocksmith2014-Mod-Installer/GUI.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace RS2014_Mod_Installer
+{
+    partial class GUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GUI));
+            this.UseModsButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UseModsButton
+            // 
+            this.UseModsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 64F, System.Drawing.FontStyle.Bold);
+            this.UseModsButton.Location = new System.Drawing.Point(12, 71);
+            this.UseModsButton.Name = "UseModsButton";
+            this.UseModsButton.Size = new System.Drawing.Size(911, 443);
+            this.UseModsButton.TabIndex = 1;
+            this.UseModsButton.Text = "Click To Use Mods";
+            this.UseModsButton.UseVisualStyleBackColor = true;
+            this.UseModsButton.Click += new System.EventHandler(this.UseCDLCButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(206, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(518, 40);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Note: You only need to open this, and press this button once.\r\nOnce it\'s done, yo" +
+    "u can delete the program from your computer.";
+            // 
+            // GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(935, 526);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UseModsButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GUI";
+            this.Text = "Rocksmith Mods Installer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button UseModsButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Rocksmith2014-Mod-Installer/GenUtil.cs
@@ -0,0 +1,209 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+#pragma warning disable IDE0059 // "You made this variable and didn't use it". It's called future proofing.
+#pragma warning disable IDE0029 // "Null Check can be simplified"... Yeah we know, but it works so why should we mess with it.
+
+namespace RSMods.Util
+{
+    public static class GenUtil
+    {
+        public static bool IsDirectoryEmpty(string path)
+        {
+            return !Directory.EnumerateFileSystemEntries(path).Any();
+        }
+
+        public static void ExtractEmbeddedResource(string outputDir, Assembly resourceAssembly, string resourceLocation, string[] files)
+        {
+            if (!Directory.Exists(outputDir))
+                Directory.CreateDirectory(outputDir);
+
+            string[] names = resourceAssembly.GetManifestResourceNames();
+
+            string resourcePath = String.Empty;
+            foreach (string file in files)
+            {
+                resourcePath = Path.Combine(outputDir, file);
+
+                Stream stream = resourceAssembly.GetManifestResourceStream(String.Format("{0}.{1}", resourceLocation, file));
+                using (FileStream fileStream = new FileStream(resourcePath, FileMode.Create))
+                    stream.CopyTo(fileStream);
+            }
+        }
+
+        private static bool IsRSFolder(this string folderPath)
+        {
+            if (!Directory.Exists(folderPath))
+                return false;
+
+            string dlcFolderPath = Path.Combine(folderPath, "dlc");
+            string cachePsarcPath = Path.Combine(folderPath, "cache.psarc");
+
+            if (IsDirectoryEmpty(dlcFolderPath))
+                return false;
+
+            if (!File.Exists(cachePsarcPath))
+                return false;
+
+            return true;
+        }
+
+        private static string GetStringValueFromRegistry(string keyName, string valueName)
+        {
+            try
+            {
+                var retValue = (string)Registry.GetValue(keyName, valueName, "");
+                return retValue == null ? String.Empty : retValue;
+            }
+            catch (Exception)
+            {
+                return String.Empty;
+            }
+        }
+        private static List<string> GetCustomSteamappsFolders(string mainSteamPath)
+        {
+            string libRegex = "(^\\t\"[1-9]\").*(\".*\")";
+            var libDirs = new List<string>();
+
+            string steamappsFolder = Path.Combine(mainSteamPath, "steamapps");
+            string libVdf = Path.Combine(steamappsFolder, "libraryfolders.vdf");
+
+            if (!File.Exists(libVdf))
+                return new List<string>();
+
+            var content = File.ReadAllLines(libVdf);
+            foreach (string l in content)
+            {
+                var reg = Regex.Match(l, libRegex);
+                string dir = reg.Groups[2].Value;
+
+                if (dir != string.Empty)
+                {
+                    string ndir = dir.Trim('\"');
+                    libDirs.Add(ndir);
+                }
+            }
+
+            if (libDirs.Count == 0)
+                return new List<string>();
+
+            return libDirs;
+        }
+
+
+        private static string GetCustomRSFolder(string mainSteamPath)
+        {
+            var customSteamppsFolders = GetCustomSteamappsFolders(mainSteamPath);
+            string finalPath = string.Empty;
+            string rsFolderPath = string.Empty;
+            bool found = false;
+
+            foreach (var customSteamappsFolder in customSteamppsFolders)
+            {
+                string dirPath = Path.Combine(customSteamappsFolder, "steamapps", "appmanifest_221680.acf");
+
+                if (File.Exists(dirPath))
+                {
+                    finalPath = Path.GetDirectoryName(dirPath);
+
+                    rsFolderPath = Path.Combine(finalPath, "common", "Rocksmith2014");
+
+                    if (rsFolderPath.IsRSFolder())
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+            }
+
+            if (found)
+            {
+                //  MessageBox.Show("Found Custom RS2014 Installation Directory ...");
+                return rsFolderPath;
+            }
+
+            // MessageBox.Show("<WARNING> Custom RS2014 Installation Directory not found ...");
+            return String.Empty;
+        }
+
+        public static string GetSteamDirectory()
+        {
+            const string steamRegPath = @"HKEY_CURRENT_USER\SOFTWARE\Valve\Steam"; //IIRC it isn't the same on X86 machines, but do we really need to support those?
+
+            return GetStringValueFromRegistry(steamRegPath, "SteamPath").Replace('/', '\\');
+        }
+
+        public static List<Tuple<string, string>> InstallRegKeys = new List<Tuple<string, string>>()
+        {
+            new Tuple<string, string>(@"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Ubisoft\Rocksmith2014", "installdir"),
+            new Tuple<string, string>(@"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 221680", "InstallLocation"),
+            new Tuple<string, string>(@"HKEY_LOCAL_MACHINE\SOFTWARE\Ubisoft\Rocksmith2014", "InstallLocation"),
+            new Tuple<string, string>(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 221680", "InstallLocation")
+        };
+
+        public static string GetRSDirectory()
+        {
+            try
+            {
+                var rs2RootDir = String.Empty;
+                var steamRootPath = GetSteamDirectory();
+
+                if (!String.IsNullOrEmpty(steamRootPath))
+                {
+                    rs2RootDir = Path.Combine(steamRootPath, "SteamApps\\common\\Rocksmith2014");
+
+                    if (!Directory.Exists(rs2RootDir))
+                    {
+                        // Go through each possible registry location
+                        foreach (var installRegKey in InstallRegKeys)
+                        {
+                            var path = GetStringValueFromRegistry(installRegKey.Item1, installRegKey.Item2);
+
+                            if (!string.IsNullOrEmpty(path))
+                            {
+                                rs2RootDir = path;
+                                break;
+                            }
+                        }
+
+                        rs2RootDir = GetCustomRSFolder(steamRootPath); // Grab custom Steam library paths from .vdf file
+
+                        if (String.IsNullOrEmpty(rs2RootDir) || !rs2RootDir.IsRSFolder()) // If neither that's OK, ask the user to point the GUI to the correct location
+                        {
+                            MessageBox.Show("We were unable to detect your Rocksmith 2014 folder, please select it manually!", "Your help is required!");
+                            using (CommonOpenFileDialog dialog = new CommonOpenFileDialog()) // FolderBrowserDialog lacks usability, while using OpenFileDialog can be a bit wonky so this is likely the best solution
+                            {
+                                dialog.IsFolderPicker = true;
+                                if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+                                {
+                                    string rsFolder = dialog.FileName;
+
+                                    if (rsFolder.IsRSFolder())
+                                        return rsFolder;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                return rs2RootDir;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("<Warning> GetStreamDirectory, " + ex.Message);
+            }
+
+            return String.Empty;
+        }
+    }
+}
