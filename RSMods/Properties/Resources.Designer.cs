fc3acd2056a823b7a10d1e47674b970dfb48584f
ffio1 <ffioathome@gmail.com>
2020-08-13 17:00:13 -0700

    ++Version | No shipping fretboard image

--- RSMods/Properties/Resources.Designer.cs
+++ RSMods/Properties/Resources.Designer.cs
@@ -61,16 +61,6 @@ namespace RSMods.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap fretboard {
-            get {
-                object obj = ResourceManager.GetObject("fretboard", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon Logo {
