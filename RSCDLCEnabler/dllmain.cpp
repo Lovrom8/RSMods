9d209aee956fe3ea64a1a6a09cbc4849069e3cef
ffio1 <ffioathome@gmail.com>
2020-09-05 17:58:18 -0700

    Single IF statement

--- RSCDLCEnabler/dllmain.cpp
+++ RSCDLCEnabler/dllmain.cpp
@@ -415,8 +415,7 @@ HRESULT __stdcall Hook_EndScene(IDirect3DDevice9* pDevice) {
 			MemHelpers::DX9DrawText("Current Song Volume: " + std::to_string((int)songVolume), whiteText, (MemHelpers::GetWindowSize()[0] / 96), (MemHelpers::GetWindowSize()[1] / 72), FW_NORMAL, (MemHelpers::GetWindowSize()[0] / 54.85), (MemHelpers::GetWindowSize()[1] / 30.85), (MemHelpers::GetWindowSize()[0] / 14.22), (MemHelpers::GetWindowSize()[1] / 8), pDevice);
 		}
 
-		if (showSongTimerOnScreen) {
-			if (MemHelpers::ShowSongTimer() != "") {
+		if (showSongTimerOnScreen && MemHelpers::ShowSongTimer() != "") {
 				std::string currentSongTimeString = MemHelpers::ShowSongTimer();
 				size_t stringSize;
 
@@ -434,7 +433,6 @@ HRESULT __stdcall Hook_EndScene(IDirect3DDevice9* pDevice) {
 				}
 
 				MemHelpers::DX9DrawText(std::to_string(hours) + ":" + std::to_string(minutes) + ":" + std::to_string(seconds), whiteText, (MemHelpers::GetWindowSize()[0] / 96), (MemHelpers::GetWindowSize()[1] / 72), FW_NORMAL, (MemHelpers::GetWindowSize()[0] / 1.07), (MemHelpers::GetWindowSize()[1] / 30.85), (MemHelpers::GetWindowSize()[0] / 1.13), (MemHelpers::GetWindowSize()[1] / 8), pDevice);
-			}
 		}
 	}
 
