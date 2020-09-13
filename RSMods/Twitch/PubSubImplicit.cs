bd6ddc877a94a2856113f9b8b9fc6950b0526de5
ffio1 <ffioathome@gmail.com>
2020-09-12 19:51:40 -0700

    ReadOnly to appease the IntelliSense Gods

--- RSMods/Twitch/PubSubImplicit.cs
+++ RSMods/Twitch/PubSubImplicit.cs
@@ -19,12 +19,12 @@ namespace RSMods.Twitch
             RunServer();
         }
 
-        private static string twitchClientId = "6j445pcbepas4mtlzgfiset3u6symq";
-        private static string twitchRedirectUri = "http://localhost:1069/twitch_login";
-        private static string twitchScope = "chat:read bits:read channel:read:hype_train channel:read:subscriptions channel:read:redemptions";
-        private static string responseForwardContent =  "<HTML><BODY> <script> location.href = window.location.href.replace(\"#\", \"/\");  </script></BODY></HTML>";
-        private static string responseFinalPageContent = "<HTML><BODY><h1>thanks for the token xd</h1><br><h6><i>This response was totally not suggested by Koko</i></h6></BODY></HTML>";
-        private static string url = String.Format("https://id.twitch.tv/oauth2/authorize?response_type=token&client_id={0}&redirect_uri=http://localhost:1069/twitch_login&scope={1}", twitchClientId, twitchScope);
+        private static readonly string twitchClientId = "6j445pcbepas4mtlzgfiset3u6symq";
+        private static readonly string twitchRedirectUri = "http://localhost:1069/twitch_login";
+        private static readonly string twitchScope = "chat:read bits:read channel:read:hype_train channel:read:subscriptions channel:read:redemptions";
+        private static readonly string responseForwardContent =  "<HTML><BODY> <script> location.href = window.location.href.replace(\"#\", \"/\");  </script></BODY></HTML>";
+        private static readonly string responseFinalPageContent = "<HTML><BODY><h1>thanks for the token xd</h1><br><h6><i>This response was totally not suggested by Koko</i></h6></BODY></HTML>";
+        private static readonly string url = String.Format("https://id.twitch.tv/oauth2/authorize?response_type=token&client_id={0}&redirect_uri=http://localhost:1069/twitch_login&scope={1}", twitchClientId, twitchScope);
         private static string accessToken;
 
         public void MakeRequest()
