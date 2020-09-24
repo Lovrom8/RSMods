fb43ecdb5160205a210909cdd7e01c1725532be6
Lovrom8 <lovro.posaric@gmail.com>
2020-09-24 18:10:33 +0200

    v1.1.26.2

--- Rocksmith2014-Mod-Installer/Properties/AssemblyInfo.cs
+++ Rocksmith2014-Mod-Installer/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-//
+// 
 [assembly: AssemblyVersion("1.1.26")]
 [assembly: AssemblyFileVersion("1.1.26")]
 
