8613e81e2196f7a1824e1bc7d07583078dcc85d0
Lovrom8 <pamet.pijetlova@gmail.com>
2020-09-26 00:28:48 +0200

    v1.1.30

--- Rocksmith2014-Mod-Installer/Properties/AssemblyInfo.cs
+++ Rocksmith2014-Mod-Installer/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 // 
-[assembly: AssemblyVersion("1.1.29")]
-[assembly: AssemblyFileVersion("1.1.29")]
+[assembly: AssemblyVersion("1.1.30")]
+[assembly: AssemblyFileVersion("1.1.30")]
 
