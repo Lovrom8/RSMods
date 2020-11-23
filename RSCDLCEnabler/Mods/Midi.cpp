90c16539cfa77ea4de77e329a4fc37c7fcb02ec6
ffio1 <ffio1@users.noreply.github.com>
2020-11-23 01:34:20 -0800

    More Verbose MIDI

--- RSCDLCEnabler/Mods/Midi.cpp
+++ RSCDLCEnabler/Mods/Midi.cpp
@@ -1,5 +1,7 @@
 #include "Midi.hpp"
 
+// Midi codes should follow this guide: http://fmslogo.sourceforge.net/manual/midi-table.html
+
 bool Midi::SendMidiMessage(char programChange, char toeDown) {
     RtMidiOut* midiout = new RtMidiOut();
     std::vector<unsigned char> message;
@@ -7,7 +9,7 @@ bool Midi::SendMidiMessage(char programChange, char toeDown) {
     // Check available ports.
     unsigned int nPorts = midiout->getPortCount();
     if (nPorts == 0) {
-        std::cout << "No MIDI ports available!\n";
+        std::cout << "No MIDI ports available!" << std::endl;
         delete midiout;
         return false;
     }
@@ -18,6 +20,7 @@ bool Midi::SendMidiMessage(char programChange, char toeDown) {
     if (programChange != NULL) {
         message.push_back(programChangeStatus);
         message.push_back(programChange);
+        std::cout << "Sending Midi Message: " << "PC: " << message.back() << std::endl;
         midiout->sendMessage(&message);
     }
 
@@ -25,6 +28,7 @@ bool Midi::SendMidiMessage(char programChange, char toeDown) {
         message[0] = controlChangeStatus;
         message[1] = WHAMMY_DT_CC_CHANNEL;
         message[2] = toeDown;
+        std::cout << "Sending Midi Message: " << "CC: " << message[1] << " " << message[2] << std::endl;
         midiout->sendMessage(&message);
     }
 
@@ -36,6 +40,7 @@ bool Midi::SendMidiMessage(char programChange, char toeDown) {
 void Midi::InitMidi() {
     try {
         RtMidiIn midiin;
+        std::cout << "Starting Midi" << std::endl;
     }
     catch (RtMidiError& error) {
         std::cout << error.getMessage() << std::endl;
