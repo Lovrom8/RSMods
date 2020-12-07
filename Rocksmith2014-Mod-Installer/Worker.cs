93e23b6691a3700dcdd4182c4983c7975e75011b
Lovrom8 <pamet.pijetlova@gmail.com>
2020-12-07 23:03:20 +0100

    Final build fix

--- Rocksmith2014-Mod-Installer/Worker.cs
+++ Rocksmith2014-Mod-Installer/Worker.cs
@@ -35,9 +35,7 @@ namespace RS2014_Mod_Installer
                 File.WriteAllBytes(Path.Combine(rootModFolder, "7z.dll"), Properties.Resources._7z);
                 File.WriteAllBytes(Path.Combine(rootModFolder, "ICSharpCode.SharpZipLib.dll"), Properties.Resources.ICSharpCode_SharpZipLib);
                 File.WriteAllBytes(Path.Combine(rootModFolder, "Microsoft.WindowsAPICodePack.dll"), Properties.Resources.Microsoft_WindowsAPICodePack);
-                File.WriteAllText(Path.Combine(rootModFolder, "Microsoft.WindowsAPICodePack.xml"), Properties.Resources.Microsoft_WindowsAPICodePack2);
                 File.WriteAllBytes(Path.Combine(rootModFolder, "Microsoft.WindowsAPICodePack.Shell.dll"), Properties.Resources.Microsoft_WindowsAPICodePack_Shell);
-                File.WriteAllText(Path.Combine(rootModFolder, "Microsoft.WindowsAPICodePack.Shell.xml"), Properties.Resources.Microsoft_WindowsAPICodePack_Shell2);
                 File.WriteAllBytes(Path.Combine(rootModFolder, "MiscUtil.dll"), Properties.Resources.MiscUtil);
                 File.WriteAllBytes(Path.Combine(rootModFolder, "NDesk.Options.dll"), Properties.Resources.NDesk_Options);
                 File.WriteAllBytes(Path.Combine(rootModFolder, "Newtonsoft.Json.dll"), Properties.Resources.Newtonsoft_Json);
