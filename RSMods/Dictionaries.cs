3d6e36f72c4dd1fc72dce0dffd329596b2be9efa
ffio1 <ffio1@users.noreply.github.com>
2021-01-01 23:51:01 -0800

    Tooltip change

--- RSMods/Dictionaries.cs
+++ RSMods/Dictionaries.cs
@@ -114,8 +114,8 @@ namespace RSMods
             TooltipDictionary.Add(checkBox_RevealTwitchAuthToken, "Only reveal this when asked by RSMods developers.\nThis is how we look to see when events happen in your stream.");
 
             // Custom Noteway Colors
-            TooltipDictionary.Add(button_ChangeNumberedFrets, "This will change the color of all numbered frets in game.\n3, 5, 7, 9, 12, 15, 17, 19, 21, 24");
-            TooltipDictionary.Add(button_ChangeUnNumberedFrets, "This will change the color of all NON-numbered frets in game.");
+            TooltipDictionary.Add(button_ChangeNumberedFrets, "This will change the color of all numbered frets in game.\n3, 5, 7, 9, 12, 15, 17, 19, 21, 24.\nThis needs to be paired with Change UnNumbered Frets or it won't work!");
+            TooltipDictionary.Add(button_ChangeUnNumberedFrets, "This will change the color of all NON-numbered frets in game.\nThis needs to be paired with Change Numbered Frets or it won't work!");
             TooltipDictionary.Add(button_ChangeNotewayGutter, "This will change what the sides of the noteway are colored with.");
             TooltipDictionary.Add(button_ChangeFretNumber, "This will change the color of the fret numbers that show up on the noteway.\nThis doesn't change the orange note numbers!");
 
