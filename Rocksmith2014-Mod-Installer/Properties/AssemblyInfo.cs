9c1a4ddeb813e203babdd842dddf51bd4d189430
ffio1 <ffio1@users.noreply.github.com>
2021-07-03 19:05:34 -0700

    1.2.5.0
    EXPECT BUGS
    
    - Added ToggleExtendedRangeKey
    - Moved Executable to 64-bit to add support for the following
    - Custom Result Screen Sound Pack (Get rid of the Rocksmith Dad telling you "Bad Performance")
    - Fixed bug with Custom Note Colors that caused colors to look pale
    - Twitch Crowd Control Support! Please use this over the built-in Twitch controller in the RSMods GUI!
    - Fixed some mods related to Twitch Crowd Control
    - Fixed Midi Auto Tuning not working on some songs, and pedal configurations
    - Fixed Rocksmith.ini settings being reverted back to default, and added better handling of settings being set wrong.
    - Added lists to the "Set And Forget Mods" tab that show what songs may show up as "Custom Tuning" and what songs have the tuning you have selected
    - Added a way to configure what tuning you want your guitar to stay in when using MIDI Auto Tuning
    - Fixed AutoLoadProfile not stopping properly if it can't detect a profile.
    - Added a way to fix Extended Range mode for bass songs with the wrong tuning.
    
    I hope everyone is looking forward to RS+, I know we all are. Expect updates to slow down for RSMods as we start to make some new stuff for RS+. We already have a mod that works, but there's no point in releasing it just yet because any update to the beta will cause it to break.

--- Rocksmith2014-Mod-Installer/Properties/AssemblyInfo.cs
+++ Rocksmith2014-Mod-Installer/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 // 
-[assembly: AssemblyVersion("1.2.4.1")]
-[assembly: AssemblyFileVersion("1.2.4.1")]
+[assembly: AssemblyVersion("1.2.5.0")]
+[assembly: AssemblyFileVersion("1.2.5.0")]
 
