4fb3436adf707df36ec688e49f7eee625d845587
ffio1 <ffio1@users.noreply.github.com>
2021-12-17 22:06:12 -0800

    Prevent ImGui from trying to re-open RSMods
    Default ImGui install has Gamepad support and tries to open an xinput dll, but we are the xinput dll, so it would try to open itself and can be logged as the reason for a "crash" when the user closes the game.

--- RSMods/Lib/ImGUI/imconfig.h
+++ RSMods/Lib/ImGUI/imconfig.h
@@ -114,6 +114,8 @@
 //---- Debug Tools: Enable slower asserts
 //#define IMGUI_DEBUG_PARANOID
 
+#define IMGUI_IMPL_WIN32_DISABLE_GAMEPAD
+
 //---- Tip: You can add extra functions within the ImGui:: namespace, here or in your own headers files.
 /*
 namespace ImGui
