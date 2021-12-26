0ec4f88721a7e1b766fa07ff39b70d3e872cc643
ffio1 <ffio1@users.noreply.github.com>
2021-12-26 11:11:51 -0800

    1.2.6.4

--- Rocksmith2014-Mod-Installer/Properties/AssemblyInfo.cs
+++ Rocksmith2014-Mod-Installer/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 // 
-[assembly: AssemblyVersion("1.2.6.3")]
-[assembly: AssemblyFileVersion("1.2.6.3")]
+[assembly: AssemblyVersion("1.2.6.4")]
+[assembly: AssemblyFileVersion("1.2.6.4")]
 
