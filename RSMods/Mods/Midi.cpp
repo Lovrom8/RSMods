6711af3e20356c12ecff185bc85778c1e26bf205
ffio1 <ffio1@users.noreply.github.com>
2021-12-31 12:06:34 -0800

    Replace Midi Wah effect with Midi RR Speed > 100%

--- RSMods/Mods/Midi.cpp
+++ RSMods/Mods/Midi.cpp
@@ -143,25 +143,8 @@ namespace Midi {
 
 				std::cout << "(MIDI IN) CC. Controller# = " << (int)message->at(1) << ". Value = " << std::dec << (int)message->at(2) << std::endl;
 
-				WwiseVariables::Wwise_Sound_SetRTPCValue_Char("Pedal_USWah_Auto", 1, 0x1234, 0, AkCurveInterpolation_Linear);
-				WwiseVariables::Wwise_Sound_SetRTPCValue_Char("Pedal_USWah_Auto", 1, AK_INVALID_GAME_OBJECT, 0, AkCurveInterpolation_Linear);
-				WwiseVariables::Wwise_Sound_SetRTPCValue_Char("Pedal_USWah_Sens", ceil((int)message->at(2) / 127.f * 100.f), 0x1234, 0, AkCurveInterpolation_Linear);
-				WwiseVariables::Wwise_Sound_SetRTPCValue_Char("Pedal_USWah_Sens", ceil((int)message->at(2) / 127.f * 100.f), AK_INVALID_GAME_OBJECT, 0, AkCurveInterpolation_Linear);
-
-				WwiseVariables::Wwise_Sound_SetRTPCValue_Char("Bass_Pedal_BassWah_Auto", 1, 0x1234, 0, AkCurveInterpolation_Linear);
-				WwiseVariables::Wwise_Sound_SetRTPCValue_Char("Bass_Pedal_BassWah_Auto", 1, AK_INVALID_GAME_OBJECT, 0, AkCurveInterpolation_Linear);
-				WwiseVariables::Wwise_Sound_SetRTPCValue_Char("Bass_Pedal_BassWah_Sens", ceil((int)message->at(2) / 127.f * 100.f), 0x1234, 0, AkCurveInterpolation_Linear);
-				WwiseVariables::Wwise_Sound_SetRTPCValue_Char("Bass_Pedal_BassWah_Sens", ceil((int)message->at(2) / 127.f * 100.f), AK_INVALID_GAME_OBJECT, 0, AkCurveInterpolation_Linear);
-
-				WwiseVariables::Wwise_Sound_SetRTPCValue_Char("Pedal_ModernWah_Auto", 1, 0x1234, 0, AkCurveInterpolation_Linear);
-				WwiseVariables::Wwise_Sound_SetRTPCValue_Char("Pedal_ModernWah_Auto", 1, AK_INVALID_GAME_OBJECT, 0, AkCurveInterpolation_Linear);
-				WwiseVariables::Wwise_Sound_SetRTPCValue_Char("Pedal_ModernWah_Sens", ceil((int)message->at(2) / 127.f * 100.f), 0x1234, 0, AkCurveInterpolation_Linear);
-				WwiseVariables::Wwise_Sound_SetRTPCValue_Char("Pedal_ModernWah_Sens", ceil((int)message->at(2) / 127.f * 100.f), AK_INVALID_GAME_OBJECT, 0, AkCurveInterpolation_Linear);
-
-				WwiseVariables::Wwise_Sound_SetRTPCValue_Char("Pedal_UKWah_Auto", 1, 0x1234, 0, AkCurveInterpolation_Linear);
-				WwiseVariables::Wwise_Sound_SetRTPCValue_Char("Pedal_UKWah_Auto", 1, AK_INVALID_GAME_OBJECT, 0, AkCurveInterpolation_Linear);
-				WwiseVariables::Wwise_Sound_SetRTPCValue_Char("Pedal_UKWah_Sens", ceil((int)message->at(2) / 127.f * 100.f), 0x1234, 0, AkCurveInterpolation_Linear);
-				WwiseVariables::Wwise_Sound_SetRTPCValue_Char("Pedal_UKWah_Sens", ceil((int)message->at(2) / 127.f * 100.f), AK_INVALID_GAME_OBJECT, 0, AkCurveInterpolation_Linear);
+				RiffRepeater::SetSpeed((message->at(2) * 2), true);
+				RiffRepeater::EnableTimeStretch();
 				break;
 			case MidiCommands::PC:
 				if (messageSize != 2) {
