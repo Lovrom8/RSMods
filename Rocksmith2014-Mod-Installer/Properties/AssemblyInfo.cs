af5ac78fa0d7cde9ce2631baab4d15fe311f80a7
Lovro Posarić <lovro.posaric@gmail.com>
2022-09-24 22:52:38 +0200

    1.2.7.2

--- Rocksmith2014-Mod-Installer/Properties/AssemblyInfo.cs
+++ Rocksmith2014-Mod-Installer/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 // 
-[assembly: AssemblyVersion("1.2.6.5")]
-[assembly: AssemblyFileVersion("1.2.6.5")]
+[assembly: AssemblyVersion("1.2.7.2")]
+[assembly: AssemblyFileVersion("1.2.7.2")]
 
